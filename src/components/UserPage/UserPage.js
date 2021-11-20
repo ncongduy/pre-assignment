ce8282af3fd33c5041ff955aced60ad080e574da
Nguyen Cong Duy <ncongduy@gmail.com>
2021-11-20 06:39:04 +0200

    chore: separate layout and content - component UserPage

--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -16,21 +16,23 @@ export default function UserPage({ data }) {
 
 	return (
 		<Container>
-			<Alert variant={'info'} className={styles.content}>
-				<p>Name: {dataRender.name}</p>
-				<p>Username: {dataRender.username}</p>
-				<p>Email: {dataRender.email}</p>
-				<p>Phone: {dataRender.phone}</p>
-				<p>Company: {dataRender.company.name}</p>
-				<p>Website: http://{dataRender.website}</p>
-				<p>Address:</p>
-				<ul>
-					<li>Street: {dataRender.address.street}</li>
-					<li>Suite: {dataRender.address.suite}</li>
-					<li>City: {dataRender.address.city}</li>
-					<li>Zipcode: {dataRender.address.zipcode}</li>
-				</ul>
-			</Alert>
+			<div className={styles.content}>
+				<Alert variant={'info'}>
+					<p>Name: {dataRender.name}</p>
+					<p>Username: {dataRender.username}</p>
+					<p>Email: {dataRender.email}</p>
+					<p>Phone: {dataRender.phone}</p>
+					<p>Company: {dataRender.company.name}</p>
+					<p>Website: http://{dataRender.website}</p>
+					<p>Address:</p>
+					<ul>
+						<li>Street: {dataRender.address.street}</li>
+						<li>Suite: {dataRender.address.suite}</li>
+						<li>City: {dataRender.address.city}</li>
+						<li>Zipcode: {dataRender.address.zipcode}</li>
+					</ul>
+				</Alert>
+			</div>
 		</Container>
 	);
 }
